091f416e42f92e7ead730820027aaa2ea4f620b2
pashupat-git <pashupatic.github@gmail.com>
2025-12-30 14:18:16 +0545

    app name changed

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PawPal',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
